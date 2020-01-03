2f63585f12ec203164d012b0030ba4cccc99f8fb
M.Zubok <maxym.zubok@gmail.com>
2020-01-03 09:39:55 +0200

    Commit implemantation of Service and Repository

---
+++ CleanArch/CleadArch.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using ClearArch.Infrastructure.Data.Context;
+
+namespace ClearArch.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
